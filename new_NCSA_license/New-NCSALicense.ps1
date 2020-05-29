86eb300174cd514bf3e13eb8797bdbd58dcead13
MKletz <michael.kletz.27@gmail.com>
2020-05-29 14:57:03 -0500

    moved template into ps1 file

--- new_NCSA_license/New-NCSALicense.ps1
+++ new_NCSA_license/New-NCSALicense.ps1
@@ -1,3 +1,29 @@
+<#
+.Synopsis
+   The NCSA license has a few project specific spots in the template. This just swaps out that information appropriately.
+.DESCRIPTION
+   The NCSA license has a few project specific spots in the template. This just swaps out that information appropriately.
+.PARAMETER TeamName
+    For our purposes this is typically "Cybersecurity <<sub team>>". Ex: Cybersecurity Engineering.
+.PARAMETER RepositoryURL
+    The public URL for your project's repository.
+.PARAMETER Path
+    Path to create the license file at. Defaulted to 'LICENSE' in your current working directory.
+.EXAMPLE
+   PS C:\> .\New-NCSALicense.ps1 -TeamName "CyberSecurity Engineering" -RepositoryURL "https://github.com/techservicesillinois/SecDev-Tools"
+#>
+
+param(
+    [Parameter(Mandatory=$true)]    
+    [string]$TeamName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$RepositoryURL,
+
+    [string]$Path = "$($PWD)\LICENSE"
+)
+
+[string]$Template = @"
 Copyright (c) {2} University of Illinois.  All rights reserved.
 
 
@@ -27,3 +53,7 @@ CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE
 SOFTWARE.
+"@
+
+$Template -f $TeamName,$RepositoryURL,([datetime]::Now.Year) | Set-Content -Path $Path -Force
+Get-Item -Path $Path
