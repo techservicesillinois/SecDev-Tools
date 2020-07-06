df31d8f9432c9d4718699225aa1ff9707301076f
MKletz <michael.kletz.27@gmail.com>
2020-07-06 11:03:08 -0500

    missed a few spots (#9)

--- plaster_templates/NewPowerShellModuleProject/test/Module.T.ps1
+++ plaster_templates/NewPowerShellModuleProject/test/Module.T.ps1
@@ -1,9 +1,9 @@
-[String]$ModuleRoot = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath '<%=$PLASTER_PARAM_ModuleName%>'
+[String]$ModuleRoot = Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'src\<%=$PLASTER_PARAM_ModuleName%>'
 Import-Module -Name $ModuleRoot
 
 Describe 'Module Manifest Tests' {
     It 'Passes Test-ModuleManifest' {
-        $ManifestPath = Join-Path -Path "$(Split-Path -Path $PSScriptRoot -Parent)" -ChildPath '<%=$PLASTER_PARAM_ModuleName%>/<%=$PLASTER_PARAM_ModuleName%>.psd1'
+        $ManifestPath = Join-Path -Path "$(Split-Path -Path $PSScriptRoot -Parent)" -ChildPath 'src\<%=$PLASTER_PARAM_ModuleName%>\<%=$PLASTER_PARAM_ModuleName%>.psd1'
         Test-ModuleManifest -Path $ManifestPath | Should -Not -BeNullOrEmpty
     }
 }
