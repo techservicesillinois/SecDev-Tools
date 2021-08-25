33cb2ffdb570f2b47b261fb547b2d24f533ffc1d
MKletz <michael.kletz.27@gmail.com>
2021-08-25 14:21:01 -0500

    Fixed regex test script (#35)

--- regex_test.ps1
+++ regex_test.ps1
@@ -1,5 +1,5 @@
 <#
-    This script will use the pester unit testing framework in PowerShell to verify a given regex pattern vs test data.
+    This script will verify a given regex pattern vs test data.
 
     This will execute the tests via the .Net regex engine so keep in mind it might not be a perfect port to your given language
     as not all parts of the engine are supported universally. Ex: negative lookahead is supported in .Net but not Go.
@@ -10,22 +10,29 @@
     i11in0is.mydomain.site,1
 
     While pattern.txt is just a single line txt file containing your pattern.
-    Both of these files should be in the same directory as this script.
-
-    The script can be executed by being called directly or by having it inside your working directory and running "Invoke-Pester"   
+    Both of these files should be in the same directory as this script. 
 #>
 
 [regex]$Pattern = (Get-Content -Path "$($PSScriptRoot)\pattern.txt")
 [System.Collections.ArrayList]$TestData = @()
 $TestData += Import-Csv -Path "$($PSScriptRoot)\testdata.csv" | Where-Object -FilterScript {$_.String} #Filter out blank lines
 
-Describe "$($Pattern.ToString())"{    
-    Foreach($Test in $TestData){      
-        $ShouldMatch = [Boolean][int]$Test.ShouldMatch
-        [String]$Verb = if($ShouldMatch){'Matches'}Else{'Ignores'}
-
-        It "$($Verb) '$($Test.String)'"{
-            $Pattern.Match($Test.String).Success | Should -Be $ShouldMatch
-        }
+Foreach($Test in $TestData){      
+    $ShouldMatch = [Boolean][int]$Test.ShouldMatch
+    
+    if($ShouldMatch){
+        [String]$Message = "Matches '$($Test.String)'"
     }
+    Else{
+        [String]$Message = "Ignores '$($Test.String)'"
+    }
+
+    if($Pattern.Match($Test.String).Success -eq $ShouldMatch){
+        $ForegroundColor = 'Green'
+    }
+    else{
+        $ForegroundColor = 'Red'
+    }
+
+    Write-Host -Object $Message -ForegroundColor $ForegroundColor
 }
